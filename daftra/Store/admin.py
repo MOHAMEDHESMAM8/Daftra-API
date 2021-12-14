e030fc8f1156448f5fda08c71c130ec117fa50fe
MOHAMEDHESMAM8 <59764426+MOHAMEDHESMAM8@users.noreply.github.com>
2021-12-14 04:55:27 +0200

    admin product count

--- daftra/Store/admin.py
+++ daftra/Store/admin.py
@@ -3,6 +3,7 @@ from  .models import *
 # Register your models here.
 admin.site.register(Warehouses)
 admin.site.register(Products)
+admin.site.register(Product_count)
 admin.site.register(Brands)
 admin.site.register(Categories)
 admin.site.register(ProductsCategory)
