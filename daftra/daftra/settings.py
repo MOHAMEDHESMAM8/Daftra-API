c5b8bc878a1747d8e121945f2ef8402c6a5b77dd
MOHAMEDHESMAM8 <59764426+MOHAMEDHESMAM8@users.noreply.github.com>
2021-12-21 00:26:38 +0200

    update settings

--- daftra/daftra/settings.py
+++ daftra/daftra/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-%k0*34(hoxo==_wh8obx@xll9r2)4!26y=n^ii0=babz%57myl'
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.getenv("DEBUG", "False") == "True"
